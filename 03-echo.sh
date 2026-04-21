351402d071e02b216b359a5cfe8b3c1858e78f34
Raghu K <raghuk.vit@gmail.com>
2026-04-21 06:35:07 +0530

    RoboShop Code

---
+++ 03-echo.sh
@@ -0,0 +1,6 @@
+echo Hello World
+
+echo -e \\e[31mHello World\\e[0m # Red Color
+echo -e "\e[31mHello World\e[0m" # prefer using double quotes
+echo -e '\e[31mHello World\e[0m' # Avoid using single quotes
+
