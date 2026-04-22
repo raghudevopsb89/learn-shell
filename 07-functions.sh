c4a4938407e48da3a1a53a9aecdbe7d39b2efb4c
Raghu K <raghuk.vit@gmail.com>
2026-04-22 06:38:42 +0530

    RoboShop Code

--- 07-functions.sh
+++ 07-functions.sh
@@ -1,9 +1,11 @@
 sample() {
   echo Hello from function
+  echo a - $a
 }
-
+a=10
 sample
 
 source common.sh
 sample1
 sample2
+echo b - $b
--- common.sh
+++ common.sh
@@ -1,8 +1,10 @@
 sample1() {
+  b=20
   echo Hello from sample1 function
 }
 
 sample2() {
+  c=30
   echo Hello from sample2 function
 }
 
