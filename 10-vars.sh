70f609ae594f107ebca7fc69d349392763dffba3
Raghu K <raghuk.vit@gmail.com>
2026-04-23 07:05:59 +0530

    RoboShop Code

--- 10-vars.sh
+++ 10-vars.sh
@@ -13,3 +13,12 @@ echo First argument to script - $1
 # After executing script
 read -p 'Enter z: ' z
 echo z from input - $z
+
+
+## Out of four, right side using arguments are preferred by many engineers to write, As this is the standard way of most commands that execute in Linux.
+
+echo First Argument - $1
+echo Second Argument - $2
+# echo Nth Argument - ${n} # beyond $9, From $10, to get the value we should use ${10}
+echo All agguments - $*
+echo Number of Arguments - $#
